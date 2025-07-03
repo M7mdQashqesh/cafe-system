d32619e3ea480b86da1d0ebf0099425709377918
Mohammed Qashqesh <mo.qashqesh@gmail.com>
2025-07-03 14:17:34 +0300

    refactor: organize and clean up code in the home page

--- scripts/main.js
+++ scripts/main.js
@@ -3,39 +3,39 @@ cart.addEventListener("click", function () {
   window.location.href = "../pages/cart.html";
 });
 
+// Get Data From JSON Files
 async function getData(link, place) {
   let productsArea = document.querySelector(`.varieties .${place} .products`);
 
   let allData = await fetch(link);
   allData = await allData.json();
+
+  // Create Products Card
   for (let i = 0; i < allData.length; i++) {
-    let div = document.createElement("div");
-    div.className = "product";
+    let productDiv = document.createElement("div");
+    productDiv.className = "product";
 
     let imageContainer = document.createElement("div");
-    imageContainer.style.backgroundImage = `url(${allData[i].src})`;
-    imageContainer.style.backgroundSize = "cover";
-    imageContainer.style.backgroundPosition = "center";
-    imageContainer.style.width = "100%";
-    imageContainer.style.height = "220px";
-    div.appendChild(imageContainer);
+    imageContainer.style.cssText = `background-image: url(${allData[i].src}); background-size:cover; background-position: center; width: 100%; height: 220px`;
+    productDiv.appendChild(imageContainer);
 
     let productName = document.createElement("p");
     productName.className = "product-name";
     productName.textContent = allData[i].name;
-    div.appendChild(productName);
+    productDiv.appendChild(productName);
 
     let description = document.createElement("p");
-    description.className = "long-description";
+    description.className = "description";
     description.textContent = allData[i].description;
-    div.appendChild(description);
+    productDiv.appendChild(description);
 
     let productPrice = document.createElement("p");
     productPrice.className = "product-price";
     productPrice.textContent = `${allData[i].price} ₪`;
-    div.appendChild(productPrice);
+    productDiv.appendChild(productPrice);
 
-    div.addEventListener("click", function () {
+    // Save selected product details in localStorage and redirect to the product details page
+    productDiv.addEventListener("click", function () {
       let pDetails = {
         src: allData[i].src,
         name: allData[i].name,
@@ -46,7 +46,7 @@ async function getData(link, place) {
       window.location.href = "../pages/productDetails.html";
     });
 
-    productsArea.appendChild(div);
+    productsArea.appendChild(productDiv);
   }
 }
 getData("../jsons/drinks.json", "drinks");
@@ -56,20 +56,27 @@ let categoriesList = document.querySelectorAll(".categories ul li a");
 categoriesList.forEach((list) => {
   list.addEventListener("click", function (e) {
     e.preventDefault();
+    // Remove the "active" class from all category links
     categoriesList.forEach((btn) => {
       btn.classList.remove("active");
     });
+
+    // Add the "active" class to the clicked category link
     list.classList.add("active");
 
+    // Show both drinks and sweets if "All" category is selected
     if (e.target.textContent === "All") {
       document.querySelector(".drinks").classList.remove("hidden");
       document.querySelector(".sweets").classList.remove("hidden");
     }
 
+    // Show only drinks and hide sweets if "Drinks" category is selected
     if (e.target.textContent === "Drinks") {
       document.querySelector(".drinks").classList.remove("hidden");
       document.querySelector(".sweets").classList.add("hidden");
     }
+
+    // Show only sweets and hide drinks if "Sweets" category is selected
     if (e.target.textContent === "Sweets") {
       document.querySelector(".sweets").classList.remove("hidden");
       document.querySelector(".drinks").classList.add("hidden");
